91a7a27bd60768c9790087cc51c4d4781ca79bc3
klau5 <45886598+klau5@users.noreply.github.com>
2019-06-27 08:05:04 +0530

    -

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,16 @@ import SearchBar from './SearchBar';
 import youtube from '../api/youtube';
 
 class App extends Component {
+	state = { videos: [] };
+
 	onSearchSubmit = async search_term => {
 		const response = await youtube.get('/search', {
 			params: {
-				q: search_term
+                q: search_term,
 			}
 		});
 
-		console.log(response);
+		this.setState({ videos: response.data.items });
 	};
 
 	render() {
