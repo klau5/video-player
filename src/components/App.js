750891592db216833da3b4ae3b343e28c283bcc7
klau5 <45886598+klau5@users.noreply.github.com>
2019-06-27 05:59:37 +0530

    -

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import SearchBar from './SearchBar';
 
 class App extends Component {
 	render() {
-		return <div>Hello World!</div>;
+		return <div>SearchBar</div>;
 	}
 }
 
